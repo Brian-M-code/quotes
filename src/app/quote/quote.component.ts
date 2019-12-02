4ac5e6a8e51d193578aa76a8edcc47d59db47d94
Brian-M-Code <mutuma.brian@yahoo.com>
2019-12-02 10:28:59 +0300

    added like and dislike functions

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,8 +15,30 @@ export class QuoteComponent implements OnInit {
     new Quote('Tomashi','To get to Heaven you start from hell',new Date(2019,1,12)),
     new Quote('Mutuma','Dreaming without actualizing the dream is bullshit',new Date(2019,1,12)),
   ];
+addNewQuote(quote){
+  quote.completeDate = new Date(quote.completeDate)
+  this.quotes.push(quote)
+}
+displayDetails(index){
+  this.quotes[index].showQuotes = !this.quotes[index].showQuotes;
+}
+deleteQuote(isComplete, index){
+  if(isComplete){
+    let toDelete=confirm('Are you sure you wanna delete'+ this.quotes[index] .quote +'?')
 
+    if(toDelete){
+      this.quotes.splice(index,1)
+    }
+  }
 
+}
+
+addLikeVotes(i){
+  this.quotes[i].likeVotes+=1;
+}
+addDislikeVotes(i){
+  this.quotes[i].dislikeVotes+=1;
+}
   constructor() { }
 
   ngOnInit() {
